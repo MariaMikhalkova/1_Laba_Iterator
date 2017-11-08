663b7dc7e750c805ae56b2a9c8121c87844cf707
MariaMikhalkova <32015131+MariaMikhalkova@users.noreply.github.com>
2017-11-08 18:47:55 +0300

    add output

--- Laba1_Iterator/Iterator.cpp
+++ Laba1_Iterator/Iterator.cpp
@@ -12,6 +12,10 @@ FileIterator::FileIterator(const string & _fileMask)
 
 void FileIterator::DisplayFiles(const string& FileMask)
 {
+	//***********************************************
+	string path, name;
+	//***********************************************
+
 	_finddata_t FindData;   // уникальный дескриптор для вызова функций _findnext и _findclose
 	intptr_t    FindHandle; // структура, которая получает информацию об найденном файле или подкаталоге
 
@@ -26,15 +30,24 @@ void FileIterator::DisplayFiles(const string& FileMask)
 		if ((FindData.name == string(".")) || (FindData.name == string("..")))
 			continue;
 		cout << (IsDirectory(FindData) ? "Directory" : "File") << ": " << FindData.name << "  file size=" << FindData.size << " bytes" << endl;
+
+		//***********************************************
+		name = FindData.name;
+		path = defFileMask.substr(0, FileMask.find_last_of('\\'));
+		cout << path << "\\" << name << endl << endl;//вывод пути к файлу/папке 
+		//************************************
+
 		if ((IsDirectory(FindData)) == true)//если ето папка
 		{
 			string defFileMask1 = defFileMask.insert(defFileMask.find_last_of('\\') + 1, string(FindData.name) + '\\');//вставить в маску имя папки +\
-									
+						
 			it2 = new FileIterator(defFileMask1);//внутренний итератор с новой маской
 			if (it2->hasMore())
 			{
 				DisplayFiles(defFileMask1);
 			}
+
+
 
 		}
 	} while (_findnext(FindHandle, &FindData) == 0);
